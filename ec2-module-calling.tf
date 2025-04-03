897936961b7e411f5185b99adbb54165fc628f9d
Surekha390 <surekha.tallapalli@gmail.com>
2025-04-03 11:55:28 +0200

    calling existing module from Github

---
+++ ec2-module-calling.tf
@@ -0,0 +1,11 @@
+module "consul" {
+  source = "github.com/Surekha390/surekha-ec2-module"
+}
+
+/*
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.8.0"
+}
+*/
+#to use existing module, we can just copy and paste this code from the module page.
