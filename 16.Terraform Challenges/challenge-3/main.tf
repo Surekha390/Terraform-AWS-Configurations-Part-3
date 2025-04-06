a262349ec55149bc6b2a591ef1ea6bf64aa9dc37
Surekha390 <surekha.tallapalli@gmail.com>
2025-04-06 16:41:03 +0200

    for_each challenge

--- 16.Terraform Challenges/challenge-3/main.tf
+++ 16.Terraform Challenges/challenge-3/main.tf
@@ -1,4 +1,5 @@
 
+/*Challenge-task:
 variable "instance_config" {
   type = map
   default = {
@@ -6,4 +7,23 @@ variable "instance_config" {
     instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
   }
 }
+*/
 
+variable "instance_config" {
+  type = map
+  default = {
+    instance1 = { instance_type = "t2.micro", ami = "ami-0edf4a7e3058418ad" }
+    #instance2 = { instance_type = "t2.small", ami = "ami-087f352c165340ea1" }
+    instance3 = { instance_type = "t2.small", ami = "ami-087f352c165340ea1" }
+  }
+}
+
+resource "aws_instance" "web" {
+  for_each = var.instance_config
+  ami           = each.value.ami
+  instance_type = each.value.instance_type
+  
+}
+
+
+ 
